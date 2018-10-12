23face28ad26734a4625e82e004f5ae6e34963b8
Zealoft <zealoft1115@gmail.com>
2018-10-12 19:28:39 +0800

    first

---
+++ app/pages/MainPage/Main.js
@@ -0,0 +1,125 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  PixelRatio,
+  Dimensions
+} from 'react-native';
+import {
+  Calendar,
+  CalendarList,
+  Agenda,
+  LocaleConfig
+} from 'react-native-calendars';
+
+
+// import ScreenUtil from '../ScreenUtil';
+
+
+
+// const screenWeight = ScreenUtil.width;
+// const
+
+
+
+LocaleConfig.locales['zh-CN'] = {
+  monthNames: ['一月','二月','三月','四月','五月','六月',
+  '七月','八月','九月','十月','十一月','十二月'],
+  monthNamesShort: ['一','二','三','四','五','六','七','八','九','十','十一','十二'],
+  dayNames: ['周日','周一','周二','周三','周四','周五','周六'],
+  dayNamesShort: ['日','一','二','三','四','五','六']
+};
+
+LocaleConfig.defaultLocale = 'zh-CN';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu and fuck you',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+
+
+
+      <View style={{
+        flex : 1,
+        flexDirection : 'column'}}>
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
+
+
+        <Calendar
+          // Initially visible month. Default = Date()
+          // current={'2012-03-01'}
+          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+          // minDate={'2012-05-10'}
+          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          // maxDate={'2012-05-30'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => {console.log('selected day', day)}}
+          // Handler which gets executed on day long press. Default = undefined
+          onDayLongPress={(day) => {console.log('selected day', day)}}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'yyyy MM'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => {console.log('month changed', month)}}
+          // Hide month navigation arrows. Default = false
+          hideArrows={true}
+          // Replace default arrows with custom ones (direction can be 'left' or 'right')
+          renderArrow={(direction) => (<Arrow />)}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={1}
+          // Hide day names. Default = false
+          // hideDayNames={true}
+          // Show week numbers to the left. Default = false
+          // showWeekNumbers={true}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+          onPressArrowLeft={substractMonth => substractMonth()}
+          // Handler which gets executed when press arrow icon left. It receive a callback can go next month
+          onPressArrowRight={addMonth => addMonth()}
+/>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './app/pages/MainPage/Main';
+import {name as appName} from './app.json';
+import ScreenUtil from './ScreenUtil';
+
+AppRegistry.registerComponent(appName, () => App);
