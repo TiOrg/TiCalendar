ab7f125f906150c942924099ecf3d311ccc7346b
Zealoft <zealoft1115@gmail.com>
2018-10-13 18:03:46 +0800

    更新LeanCloud API配置

--- app/pages/MainPage/Main.js
+++ app/pages/MainPage/Main.js
@@ -4,8 +4,7 @@ import {
   StyleSheet,
   Text,
   View,
-  PixelRatio,
-  Dimensions
+  ScrollView
 } from 'react-native';
 import {
   Calendar,
@@ -51,67 +50,23 @@ export default class App extends Component<Props> {
 
 
       <SafeAreaView style={styles.container}>
-        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text> */}
+        <Text style={styles.text}>Calendar with selectable date and arrows</Text>
 
-
-        <Calendar
-          // Initially visible month. Default = Date()
-          // current={'2012-03-01'}
-          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          // minDate={'2012-05-10'}
-          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          // maxDate={'2012-05-30'}
-          // Handler which gets executed on day press. Default = undefined
-          onDayPress={(day) => {console.log('selected day', day)}}
-          // Handler which gets executed on day long press. Default = undefined
-          onDayLongPress={(day) => {console.log('selected day', day)}}
-          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-          monthFormat={'yyyy MM'}
-          // Handler which gets executed when visible month changes in calendar. Default = undefined
-          onMonthChange={(month) => {console.log('month changed', month)}}
-          // Hide month navigation arrows. Default = false
-          hideArrows={true}
-          // Replace default arrows with custom ones (direction can be 'left' or 'right')
-          renderArrow={(direction) => (<Arrow />)}
-          // Do not show days of other months in month page. Default = false
-          hideExtraDays={true}
-          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-          // day from another month that is visible in calendar page. Default = false
-          disableMonthChange={true}
-          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-          firstDay={1}
-          // Hide day names. Default = false
-          // hideDayNames={true}
-          // Show week numbers to the left. Default = false
-          // showWeekNumbers={true}
-          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-          onPressArrowLeft={substractMonth => substractMonth()}
-          // Handler which gets executed when press arrow icon left. It receive a callback can go next month
-          onPressArrowRight={addMonth => addMonth()}
-/>
-
-</SafeAreaView>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  calendar: {
+   borderTopWidth: 1,
+   paddingTop: 5,
+   borderBottomWidth: 1,
+   borderColor: '#eee',
+   height: 350
+ },
+ container: {
+   flex: 1,
+   backgroundColor: 'gray'
+ }
 });
--- readme.md
+++ readme.md
@@ -11,3 +11,13 @@
 `npm install --save react-native-calendars`
 
 more and more to be added.
+
+### leanCloud 
+#### 存储服务
+
+`npm install leancloud-storage --save`
+
+#### 实时消息服务
+
+
+`$ npm install leancloud-realtime --save`
