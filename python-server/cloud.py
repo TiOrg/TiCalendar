c372ad5180beaf81b2d8f5d6a9c19fb011f3140d
张子岳 <simon0628@163.com>
2019-03-25 22:43:32 +0800

    init leancloud funcion

---
+++ python-server/cloud.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+
+from leancloud import Engine
+from leancloud import LeanEngineError
+import re
+from TimeNormalizer import TimeNormalizer
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+engine = Engine()
+
+
+@engine.define
+def parse_str(**params):
+    tn = TimeNormalizer()
+    # print("hello is called")
+    if 'str' in params:
+        text = params['str'].encode('utf-8')
+        tn.parse(text)
+        ret = []
+        for token in tn.timeToken:
+            timestr = token.time.format("YYYY-MM-DD HH:mm:ss")
+            event = re.sub(u'\d[\.、]','',token.sent).replace('\n','')
+            ret.append({'timestr':timestr, 'event':event})
+        return ret
+
+@engine.define
+def hello(**params):
+    # print("hello is called")
+    if 'name' in params:
+        return 'Hello, {}!'.format(params['name'])
+    else:
+        return 'Hello, LeanCloud!'
+
+
+@engine.before_save('Todo')
+def before_todo_save(todo):
+    content = todo.get('content')
+    if not content:
+        raise LeanEngineError('内容不能为空')
+    if len(content) >= 240:
+        todo.set('content', content[:240] + ' ...')
+
+
+@engine.define
+def refreshEvents(**params):
+    # print("hello is called")
+    if 'school' in params:
+        return 'Hello ' + params['school']
+    else:
+        return 'Hello, LeanCloud!'
+   
